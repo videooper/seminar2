cbfa0d5a39a4139d4223d96d5e914289ff2bec14
Andrey Ivanov <videooper@inbox.ru>
2022-08-12 23:39:10 +0300

    Добавили giignore файл

--- zadacha 10/Program.cs
+++ zadacha 10/Program.cs
@@ -1,10 +1,17 @@
-﻿Console.WriteLine("Введите трехзначное число.\nВыхода из программы команда Exit.");
-    while(true)
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+
+Console.Clear();
+Console.Write("Для выхода из программы команда Exit.\n или введите трёхзначное число: ");
+ while(true)
     {
         string input = Console.ReadLine();
         if(!input.Equals("exit"))
             Console.WriteLine("{0}->{1}",input, input[1]);
-        else
-            break;
+            Console.WriteLine("Вторая цифра этого числа "+ (input[1]))
+       ;
     }
-    
